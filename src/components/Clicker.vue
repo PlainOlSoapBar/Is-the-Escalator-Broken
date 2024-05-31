68475b461fa9df35192094525ee54c4f4768d402
Brandon Tseng <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-30 22:04:53 -0700

    Clicker button

---
+++ src/components/Clicker.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col space-y-4">
+        <a class="text-2xl btn btn-outline btn-accent btn-block md:btn-[644px] md:max-w-3xl md:mx-auto" target="_blank">
+            The Escalator Is Broken
+        </a>
+    </div>
+</template>
