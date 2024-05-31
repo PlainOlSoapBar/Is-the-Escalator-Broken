24c33216e359d600e0566b059b7ef1674275cce1
Brandon Tseng <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-30 21:58:17 -0700

    Updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,58 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+        screens: {
+            'xsm': '435px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px'
+        },
+		extend: {
+            colors:{
+                // Instagram Color Gradient (Orange, Red, Purple)
+                'igOrange': '#f9ce34',
+                'igRed': '#ee2a7b',
+                'igPurple': '#6228d7',
+                // Discord Blurple
+                'blurple': '#7289da',
+            },
+            fontFamily: {
+                'sans': ['"DM Sans"', 'sans-serif'],
+                'display': ['"DM Serif Display"', 'serif'],
+            },
+            fontSize:{
+                // Font size specifically for navbar
+                'nv': ['1.25rem', {
+                    lineHeight:1,
+                }],
+            },
+            typography: {
+                DEFAULT: {
+                    css: {
+                        color: '#204063',
+                        h1: {
+                            color: '#204063',
+                        },
+                        h2: {
+                            color: '#204063',
+                        },
+                        h3: {
+                            color: '#204063',
+                        },
+                        strong: {
+                            color: '#204063',
+                        },
+                    },
+                },
+            },
+        },
+	},
 
+	plugins: [
+        require('@tailwindcss/typography')
+    ],
+}
